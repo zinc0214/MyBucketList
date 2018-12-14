de9af07491291e44358041ac711840ebfa790944
A Yeon Han <ayhan@rsupport.com>
2018-12-14 16:57:32 +0900

    불필요한 화면 제거 / recyclerView 에 margin 추가

--- app/src/main/java/womenproject/com/mybury/adapter/MainBucketListAdapter.kt
+++ app/src/main/java/womenproject/com/mybury/adapter/MainBucketListAdapter.kt
@@ -19,7 +19,7 @@ import womenproject.com.mybury.view.MainFragmentDirections
  */
 
 
-// DiffUtil은 앞에서도 말했다시피 support library 24.2.0에서 추가된 클래스이다. 기존에 불편했던 RecyclerView의 효율적인 갱신 처리를 편리하게 다룰 수 있도록 제공하는 util 클래스이다.
+// DiffUtil은 support library 24.2.0에서 추가된 클래스이다. 기존에 불편했던 RecyclerView의 효율적인 갱신 처리를 편리하게 다룰 수 있도록 제공하는 util 클래스이다.
 class MainBucketListAdapter(context: Context?) : RecyclerView.Adapter<MainBucketListAdapter.ViewHolder>() {
 
     private var contextM: Context = context!!
@@ -74,3 +74,6 @@ class MainBucketListAdapter(context: Context?) : RecyclerView.Adapter<MainBucket
         return 10
     }
 }
+
+
+// TODO : ListView 의 마지막을 어떻게 다른 레이아웃으로 표시할 것인지 / 다이얼로그는 어떻게 할 것인지.
