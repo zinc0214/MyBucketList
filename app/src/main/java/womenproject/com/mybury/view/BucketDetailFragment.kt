2c990b338471debc28707cb1f8770b20c10a2489
A Yeon Han <ayhan@rsupport.com>
2019-08-06 14:02:10 +0900

    Detail 화면 구현

--- app/src/main/java/womenproject/com/mybury/view/BucketDetailFragment.kt
+++ app/src/main/java/womenproject/com/mybury/view/BucketDetailFragment.kt
@@ -1,14 +1,12 @@
 package womenproject.com.mybury.view
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.detail_image_adapter.view.*
 import womenproject.com.mybury.R
 import womenproject.com.mybury.base.BaseFragment
 import womenproject.com.mybury.databinding.FragmentBucketDetailBinding
+import womenproject.com.mybury.adapter.ImageViewPageAdapter
 import womenproject.com.mybury.viewmodels.BucketDetailViewModel
+
 
 /**
  * Created by HanAYeon on 2018. 11. 30..
@@ -22,6 +20,26 @@ class BucketDetailFragment : BaseFragment<FragmentBucketDetailBinding, BucketDet
         get() = BucketDetailViewModel()
 
     override fun initStartView() {
+
+        viewDataBinding.viewModel = viewModel
+
+        val uri1 = "https://image.shutterstock.com/image-photo/assortment-fine-chocolates-white-dark-260nw-123360676.jpg"
+        val uri2 = "https://image.shutterstock.com/image-vector/doodle-cake-happy-birthday-vector-600w-1040176828.jpg"
+        val uri3 = "https://image.shutterstock.com/image-photo/road-trip-sign-background-260nw-269296442.jpg"
+
+        val list = ArrayList<String>()
+        list.add(uri1)
+        list.add(uri2)
+        list.add(uri3)
+
+        val viewPager = viewDataBinding.moreImage.viewPager
+        val viewPagerAdapter = ImageViewPageAdapter(this.context!!, list)
+
+        viewPager.adapter = viewPagerAdapter
+
+        viewDataBinding.tabLayout.setupWithViewPager(viewPager)
+
+
 
     }
 
