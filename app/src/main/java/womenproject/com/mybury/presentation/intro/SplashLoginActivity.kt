ce662592e0182d1d9ff16017ca77604fbd04b84d
zinc <zinc0214@gmail.com>
2019-11-23 16:03:14 +0900

    구글로그인 구현

--- app/src/main/java/womenproject/com/mybury/presentation/intro/SplashLoginActivity.kt
+++ app/src/main/java/womenproject/com/mybury/presentation/intro/SplashLoginActivity.kt
@@ -6,19 +6,46 @@ import androidx.appcompat.app.AppCompatActivity
 import womenproject.com.mybury.MyBuryApplication.Companion.context
 import womenproject.com.mybury.R
 import android.os.Handler
+import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import womenproject.com.mybury.databinding.SplashWithLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class SplashLoginActivity : AppCompatActivity() {
 
     lateinit var binding: SplashWithLoginBinding
+    private val RC_SIGN_IN = 100
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
+        // [START config_signin]
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        // [END config_signin]
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
         binding = DataBindingUtil.setContentView<SplashWithLoginBinding>(this, R.layout.splash_with_login)
-        binding.loginLayout.setOnClickListener { l -> goGO() }
+        binding.loginLayout.setOnClickListener { signIn()}
 
         val hd = Handler()
         hd.postDelayed(splashhandler(), 1000)
@@ -30,11 +57,72 @@ class SplashLoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun goGO() {
+    private fun goGO(account : GoogleSignInAccount) {
+        Log.e("ayhan", "${account.displayName}, ${account.familyName}, ${account.givenName}")
         val intent = Intent(context, SplashActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         context.startActivity(intent)
         finish()
     }
 
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    goGO(account)
+                }
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.e("ayhan", "$e")
+                // ...
+            }
+        }
+    }
+
+
+    // [START auth_with_google]
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.e("ayhan", "firebaseAuthWithGoogle:" + acct.id!!)
+        // [START_EXCLUDE silent]
+        showProgressDialog()
+        // [END_EXCLUDE]
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("ayhan", "signInWithCredential:success")
+                        val user = auth.currentUser
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.e("ayhan", "signInWithCredential:failure", task.exception)
+                    }
+
+                    // [START_EXCLUDE]
+                    hideProgressDialog()
+                    // [END_EXCLUDE]
+                }
+    }
+    // [END auth_with_google]
+
+    private fun showProgressDialog() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressDialog() {
+        binding.progressBar.visibility = View.GONE
+    }
 }
