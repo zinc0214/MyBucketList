e11d595bd8e077a6272cc1cb3df5ba7fb086d24f
zinc <zinc0214@gmail.com>
2020-03-16 23:39:22 +0900

    카테고리 추가 버튼 색상 변경

--- app/src/main/java/womenproject/com/mybury/presentation/write/WriteCategoryDialogFragment.kt
+++ app/src/main/java/womenproject/com/mybury/presentation/write/WriteCategoryDialogFragment.kt
@@ -31,6 +31,7 @@ class WriteCategoryDialogFragment(private var userCategory : List<Category>,
     override fun initDataBinding() {
         viewDataBinding.addCategory.title = "카테고리 추가"
         viewDataBinding.addCategory.itemClickListener = addCategoryListener()
+        context?.resources?.getColor(R.color.write_add_item_text)?.let { viewDataBinding.addCategory.writeItemText.setTextColor(it) }
 
         for (category in userCategory) {
             addCategoryItem(category)
