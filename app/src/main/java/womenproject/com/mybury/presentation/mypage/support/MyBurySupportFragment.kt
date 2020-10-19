2b95a1df1ccd5346e78e2c936ffeb93788dbb813
zinc <zinc0214@gmail.com>
2020-10-20 00:35:34 +0900

    인앱결제 재구매 가능하도록 이슈 해결

--- app/src/main/java/womenproject/com/mybury/presentation/mypage/support/MyBurySupportFragment.kt
+++ app/src/main/java/womenproject/com/mybury/presentation/mypage/support/MyBurySupportFragment.kt
@@ -7,10 +7,12 @@ import womenproject.com.mybury.databinding.FragmentMyburySupportBinding
 import womenproject.com.mybury.presentation.base.BaseFragment
 import womenproject.com.mybury.presentation.viewmodels.BucketInfoViewModel
 
-class MyBurySupportFragment : BaseFragment<FragmentMyburySupportBinding, BucketInfoViewModel>(), PurchasesUpdatedListener, PurchaseHistoryResponseListener {
+class MyBurySupportFragment : BaseFragment<FragmentMyburySupportBinding, BucketInfoViewModel>(),
+        PurchasesUpdatedListener,
+        PurchaseHistoryResponseListener {
 
     private lateinit var billingClient: BillingClient
-    private var productIdsList: List<String>? = null
+    val skuListToQuery = ArrayList<String>()
 
     override val layoutResourceId: Int
         get() = R.layout.fragment_mybury_support
@@ -20,16 +22,25 @@ class MyBurySupportFragment : BaseFragment<FragmentMyburySupportBinding, BucketI
 
     override fun initDataBinding() {
         initBilling()
+
     }
 
+
+    // BillingClient 초기화
     private fun initBilling() {
+
+        // Google PlayConsole 의 상품Id 와 동일하게 적어준다
+        skuListToQuery.add("ice_cream")
+        skuListToQuery.add("chicken")
+
+
         billingClient = BillingClient.newBuilder(requireContext()).enablePendingPurchases().setListener(this)
                 .build()
         billingClient.startConnection(object : BillingClientStateListener {
             override fun onBillingSetupFinished(billingResult: BillingResult) {
                 if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
-                    loadProducts()
-                    queryInventoryAsync()
+                    queryPurchases()
+                    setUpView()
                 }
             }
 
@@ -39,28 +50,46 @@ class MyBurySupportFragment : BaseFragment<FragmentMyburySupportBinding, BucketI
         })
     }
 
-    private fun loadProducts() {
-        billingClient.queryPurchaseHistoryAsync(BillingClient.SkuType.INAPP, this)
+    // BillingSetUp 이 완료되었을 때 필요한 할 일 설정
+    fun queryPurchases() {
+        // BillingClient 의 준비가 되지않은 상태라면 돌려보낸다
+        if (!billingClient.isReady) {
+            Log.e("ayhan", "queryPurchases: BillingClient is not ready")
+        }
+
+        // InApp 결제에 대한 쿼리가 존재하는지 확인한다
+        val result = billingClient.queryPurchases(BillingClient.SkuType.INAPP)
+        if (result.purchasesList == null) {
+            Log.e("ayhan", "No existing in app purchases found.")
+        } else {
+            Log.e("ayhan", "Existing purchases: ${result.purchasesList}")
+        }
     }
 
-    private fun queryInventoryAsync() {
-        if (productIdsList == null) return
 
+    private fun setUpView() {
+        viewDataBinding.iceCreamPurchase.setOnClickListener { purchasItme(1) }
+        viewDataBinding.chickenPurchase.setOnClickListener { purchasItme(0) }
+    }
+
+    private fun purchasItme(listNumber: Int) {
         val params = SkuDetailsParams.newBuilder()
-        params.setSkusList(productIdsList!!).setType(BillingClient.SkuType.INAPP)
-        billingClient.querySkuDetailsAsync(
-                params.build()
-        ) { billingResult, skuDetailsList ->
-            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
-                Log.e("ayhan", "IsGoing : ${billingResult.responseCode}")
-                if (!skuDetailsList.isNullOrEmpty()) {
-                    for (item in skuDetailsList.withIndex()) {
-                        val skuItem = item.value
-                    }
-                }
+        params.setSkusList(skuListToQuery).setType(BillingClient.SkuType.INAPP)
+        billingClient.querySkuDetailsAsync(params.build()
+        ) { result, skuDetails ->
+            if (result.responseCode == BillingClient.BillingResponseCode.OK && skuDetails != null) {
+                Log.e("ayhan", "querySkuDetailsAsync : $skuDetails,,, ${skuDetails[listNumber].title}")
+                purchaseGoods(skuDetails[listNumber])
+            } else {
+                Log.e("ayhan", "No sku found from query")
             }
-            Log.e("ayhan", "IsNotGoing : ${billingResult.responseCode}")
         }
+    }
+
+    private fun purchaseGoods(skuDetail: SkuDetails) {
+        val parms = BillingFlowParams.newBuilder().setSkuDetails(skuDetail).build()
+        val responseCode = billingClient.launchBillingFlow(requireActivity(), parms)
+        Log.e("ayhan", "purchaes : ${responseCode.responseCode}, ${responseCode.debugMessage}")
     }
 
     override fun onPurchasesUpdated(billingResult: BillingResult, purchaseList: MutableList<Purchase>?) {
@@ -74,19 +103,19 @@ class MyBurySupportFragment : BaseFragment<FragmentMyburySupportBinding, BucketI
                 Log.e("ayhan", "You've cancelled the Google play billing process...")
             }
             else -> {
-                Log.e("ayhan", "Item not found or Google play billing error...")
+                Log.e("ayhan", "Item not found or Google play billing error... : ${billingResult.responseCode}")
             }
         }
     }
 
     private fun acknowledgePurchase(purchaseToken: String) {
-        val params = AcknowledgePurchaseParams.newBuilder()
-                .setPurchaseToken(purchaseToken)
-                .build()
-        billingClient.acknowledgePurchase(params) { billingResult ->
+        val consumeParams = ConsumeParams.newBuilder().setPurchaseToken(purchaseToken).build()
+
+        billingClient.consumeAsync(consumeParams) { billingResult, st ->
             if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
-                val debugMessage = billingResult.debugMessage
-                Log.e("ayhan", "Item Purchased : $debugMessage")
+                Log.e("ayhan", "Item is Go!! , ${billingResult.debugMessage} , ${billingResult.responseCode}, ${st}")
+            } else {
+                Log.e("ayhan", "Item is Go!! , ${billingResult.debugMessage}, ${billingResult.responseCode}, ${st}")
             }
         }
     }
@@ -102,5 +131,4 @@ class MyBurySupportFragment : BaseFragment<FragmentMyburySupportBinding, BucketI
         }
 
     }
-
 }
