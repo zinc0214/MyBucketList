fa6c98d0bcc0ef45e21727082982933003eb3ecc
zinc <zinc0214@gmail.com>
2020-11-12 22:23:42 +0900

    인앱결쩨 개선

--- app/src/main/java/womenproject/com/mybury/presentation/mypage/support/MyBurySupportFragment.kt
+++ app/src/main/java/womenproject/com/mybury/presentation/mypage/support/MyBurySupportFragment.kt
@@ -1,131 +1,197 @@
 package womenproject.com.mybury.presentation.mypage.support
 
 import android.util.Log
+import android.widget.Toast
 import com.android.billingclient.api.*
 import womenproject.com.mybury.R
 import womenproject.com.mybury.databinding.FragmentMyburySupportBinding
 import womenproject.com.mybury.presentation.base.BaseFragment
-import womenproject.com.mybury.presentation.viewmodels.BucketInfoViewModel
+import womenproject.com.mybury.presentation.base.BaseViewModel
 
-class MyBurySupportFragment : BaseFragment<FragmentMyburySupportBinding, BucketInfoViewModel>(),
+class MyBurySupportFragment : BaseFragment<FragmentMyburySupportBinding, BaseViewModel>(),
         PurchasesUpdatedListener,
         PurchaseHistoryResponseListener {
-
-    private lateinit var billingClient: BillingClient
-    val skuListToQuery = ArrayList<String>()
 
     override val layoutResourceId: Int
         get() = R.layout.fragment_mybury_support
 
-    override val viewModel: BucketInfoViewModel
-        get() = BucketInfoViewModel()
+    override val viewModel: BaseViewModel
+        get() = BaseViewModel()
+
+
+    private lateinit var billingClient: BillingClient
+    private val purchasableList = ArrayList<String>()
 
     override fun initDataBinding() {
-        initBilling()
+        initBillingClient()
 
     }
 
-    // BillingClient 초기화
-    private fun initBilling() {
-
-        // Google PlayConsole 의 상품Id 와 동일하게 적어준다
-        skuListToQuery.add("ice_cream")
-        skuListToQuery.add("chicken")
-
+    /**
+     *  BillingClient 초기화
+     */
+    private fun initBillingClient() {
         billingClient = BillingClient.newBuilder(requireContext()).enablePendingPurchases().setListener(this)
                 .build()
         billingClient.startConnection(object : BillingClientStateListener {
             override fun onBillingSetupFinished(billingResult: BillingResult) {
                 if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
-                   // queryPurchases()
-                    setUpView()
+                    getAcknowledgePurchasedItem()
+                    getAllPurchasedItem()
+                    setPurchasableList()
+                    setUpViews()
                 }
             }
 
             override fun onBillingServiceDisconnected() {
-                Log.e("ayhan", "Service Disconnected...")
+                Log.e("TAG", "Service Disconnected.")
             }
         })
     }
 
-    // BillingSetUp 이 완료되었을 때 필요한 할 일 설정
-    fun queryPurchases() {
+    /**
+     * 이미 결제한 적이 있는 아이템을 보여주지 않아야 하는 경우 또는
+     * 선택을 비활성화 시켜야 하는 경우 등에 사용할 function
+     * acknowledgePurchase 을 통해 구매된 제품만 확인할 수 있다.
+     */
+    fun getAcknowledgePurchasedItem() {
+
         // BillingClient 의 준비가 되지않은 상태라면 돌려보낸다
         if (!billingClient.isReady) {
-            Log.e("ayhan", "queryPurchases: BillingClient is not ready")
+            return
         }
 
-        // InApp 결제한 적이 있는 아이템으 ㅣ정보를  확인한다
-        /* val result = billingClient.queryPurchases(BillingClient.SkuType.INAPP)
-         if (result.purchasesList == null) {
-             Log.e("ayhan", "No existing in app purchases found.")
-         } else {
-             Log.e("ayhan", "Existing purchases: ${result.purchasesList}")
-         }*/
-    }
-
-    private fun setUpView() {
-        viewDataBinding.iceCreamPurchase.setOnClickListener { purchaseItem(1) }
-        viewDataBinding.chickenPurchase.setOnClickListener { purchaseItem(0) }
-    }
-
-    private fun purchaseItem(listNumber: Int) {
-        val params = SkuDetailsParams.newBuilder()
-        params.setSkusList(skuListToQuery).setType(BillingClient.SkuType.INAPP)
-        billingClient.querySkuDetailsAsync(params.build()
-        ) { result, skuDetails ->
-            if (result.responseCode == BillingClient.BillingResponseCode.OK && skuDetails != null) {
-                Log.e("ayhan", "querySkuDetailsAsync : $skuDetails,,, ${skuDetails[listNumber].title}")
-                purchaseGoods(skuDetails[listNumber])
-            } else {
-                Log.e("ayhan", "No sku found from query")
+        // 인앱결제된 내역을 확인한다
+        val result = billingClient.queryPurchases(BillingClient.SkuType.INAPP)
+        if (result.purchasesList == null) {
+            Log.d("TAG", "No existing in app purchases found.")
+        } else {
+            Log.d("TAG", "Existing Once Type Item Bought purchases: ${result.purchasesList}")
+            result.purchasesList?.forEach {
+                //결쩨된 내역에 대한 처리
             }
         }
     }
 
-    private fun purchaseGoods(skuDetail: SkuDetails) {
-        val parms = BillingFlowParams.newBuilder().setSkuDetails(skuDetail).build()
-        val responseCode = billingClient.launchBillingFlow(requireActivity(), parms)
-        Log.e("ayhan", "purchaes : ${responseCode.responseCode}, ${responseCode.debugMessage}")
+    /**
+     * 최근에 결제한 아이템을 확인하는 목적
+     * checkPurchaseHistory function 과 다르게  consumeAsync 를 통해 구매된 상품도 확인할 수 있다.
+     * !!주의!! 아이템 Id 로만 가져오기 때문에 여러번 구매하더라도 가장 최근의 제품만 가져온다.
+     */
+    fun getAllPurchasedItem() {
+        billingClient.queryPurchaseHistoryAsync(BillingClient.SkuType.INAPP, this)
     }
 
-    override fun onPurchasesUpdated(billingResult: BillingResult, purchaseList: MutableList<Purchase>?) {
+    /**
+     * 구매 가능한 리스트의 아이템을 추가한다.
+     */
+    private fun setPurchasableList() {
+
+        // Google PlayConsole 의 상품Id 와 동일하게 적어준다
+        purchasableList.add("always_item") // 중복해서 무제한으로 살 수 있는 아이템
+        purchasableList.add("one_item") // 1번만 살 수 있는 아이템
+    }
+
+
+    private fun setUpViews() {
+        //   viewDataBinding.buyOnceButton.setOnClickListener { purchaseItem(1) }
+        //   viewDataBinding.buyAlwaysButton.setOnClickListener { purchaseItem(0) }
+    }
+
+
+    /**
+     * 아이템을 구매하기 위해서는 구매가능한 아이템 리스트와 확인이 필요하다.
+     * 리스트가 존재할 경우 실제 구매를 할 수 있다.
+     */
+    private fun purchaseItem(listNumber: Int) {
+        val params = SkuDetailsParams.newBuilder()
+        params.setSkusList(purchasableList).setType(BillingClient.SkuType.INAPP)
+        billingClient.querySkuDetailsAsync(params.build()) { result, skuDetails ->
+            if (result.responseCode == BillingClient.BillingResponseCode.OK && !skuDetails.isNullOrEmpty()) {
+                val flowParams =
+                        BillingFlowParams.newBuilder().setSkuDetails(skuDetails[listNumber]).build()
+                billingClient.launchBillingFlow(requireActivity(), flowParams)
+            } else {
+                Log.e("TAG", "No sku found from query")
+            }
+        }
+    }
+
+    /**
+     * 구매 방식에 대해 처리한다. item 의 id 에 따라서
+     * purchaseAlways 또는 purchaseOnce 로 보낸다.
+     * (바텀에 결제 화면이 뜬 시점)
+     */
+    override fun onPurchasesUpdated(
+            billingResult: BillingResult,
+            purchaseList: MutableList<Purchase>?
+    ) {
         when (billingResult.responseCode) {
             BillingClient.BillingResponseCode.OK -> {
-                for (purchase in purchaseList!!) {
-                    acknowledgePurchase(purchase.purchaseToken)
+                purchaseList?.forEach {
+                    if (it.sku == "always_item") {
+                        purchaseAlways(it.purchaseToken)
+                    } else {
+                        purchaseOnce(it.purchaseToken)
+                    }
+
                 }
             }
             BillingClient.BillingResponseCode.USER_CANCELED -> {
-                Log.e("ayhan", "You've cancelled the Google play billing process...")
+                Log.d("TAG", "You've cancelled the Google play billing process...")
             }
             else -> {
-                Log.e("ayhan", "Item not found or Google play billing error... : ${billingResult.responseCode}")
+                Log.e(
+                        "TAG",
+                        "Item not found or Google play billing error... : ${billingResult.responseCode}"
+                )
             }
         }
     }
 
-    private fun acknowledgePurchase(purchaseToken: String) {
-        val consumeParams = ConsumeParams.newBuilder().setPurchaseToken(purchaseToken).build()
-
-        billingClient.consumeAsync(consumeParams) { billingResult, st ->
-            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
-                Log.e("ayhan", "Item is Go!! , ${billingResult.debugMessage} , ${billingResult.responseCode}, ${st}")
-            } else {
-                Log.e("ayhan", "Item is Go!! , ${billingResult.debugMessage}, ${billingResult.responseCode}, ${st}")
-            }
-        }
-    }
-
-    override fun onPurchaseHistoryResponse(billingResult: BillingResult, purchaseHistoryList: MutableList<PurchaseHistoryRecord>?) {
+    // 최근 구매한 아이템을 알고자 할 때 사용
+    override fun onPurchaseHistoryResponse(
+            billingResult: BillingResult,
+            purchaseHistoryList: MutableList<PurchaseHistoryRecord>?
+    ) {
         if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
             if (!purchaseHistoryList.isNullOrEmpty()) {
-                for (purchase in purchaseHistoryList) {
-                    // 구매된 목록 확인
-                    Log.e("ayhan", "is Alreay Purches = $purchase")
+                // 가장 최근에 구매된 아이템을 확인할 수 있다.
+                purchaseHistoryList.forEach {
+                    Log.d("TAG", "Previous Purchase Item : ${it.originalJson}")
                 }
             }
         }
+    }
 
+    // 소비성 (계속 구매 가능한) 제품 구매시
+    private fun purchaseAlways(purchaseToken: String) {
+        val consumeParams = ConsumeParams.newBuilder().setPurchaseToken(purchaseToken).build()
+
+        billingClient.consumeAsync(consumeParams) { billingResult, _ ->
+            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                "구매가 성공했습니다! 1 ".showToast()
+            } else {
+                Log.e("TAG", "FAIL : ${billingResult.responseCode}")
+            }
+        }
+    }
+
+    // 일회성 제품 구매시
+    private fun purchaseOnce(purchaseToken: String) {
+        val params = AcknowledgePurchaseParams.newBuilder()
+                .setPurchaseToken(purchaseToken)
+                .build()
+        billingClient.acknowledgePurchase(params) { billingResult ->
+            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                "구매가 성공했습니다!".showToast()
+            } else {
+                Log.e("TAG", "FAIL : ${billingResult.responseCode}")
+            }
+        }
+    }
+
+    private fun String.showToast() {
+        Toast.makeText(requireActivity(), this, Toast.LENGTH_SHORT).show()
     }
 }
