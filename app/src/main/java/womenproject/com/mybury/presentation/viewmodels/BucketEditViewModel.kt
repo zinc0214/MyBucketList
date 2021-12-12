0c7e7e56f43bdeed2528104aab7e7a270c890b53
hana <zinc0214@gmail.com>
2021-12-12 15:26:38 +0900

    BucketEditViewModel 의 불필요한 코드 제거
    (cherry picked from commit 1e0b58e913997f0328321e4bfdd6a9013e0cfa99)

--- app/src/main/java/womenproject/com/mybury/presentation/viewmodels/BucketEditViewModel.kt
+++ app/src/main/java/womenproject/com/mybury/presentation/viewmodels/BucketEditViewModel.kt
@@ -2,22 +2,12 @@ package womenproject.com.mybury.presentation.viewmodels
 
 import android.annotation.SuppressLint
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import womenproject.com.mybury.data.BucketItem
 import womenproject.com.mybury.data.network.apiInterface
 import womenproject.com.mybury.presentation.base.BaseViewModel
 
 class BucketEditViewModel : BaseViewModel() {
-
-    private val _apiState = MutableLiveData<ApiState>()
-    val apiState: LiveData<ApiState> get() = _apiState
-
-    private val _bucketList = MutableLiveData<List<BucketItem>>()
-    val bucketList: LiveData<List<BucketItem>> get() = _bucketList
-
 
     @SuppressLint("CheckResult")
     fun getMainBucketList(callback: MoreCallBackAny) {
@@ -50,45 +40,5 @@ class BucketEditViewModel : BaseViewModel() {
                 Log.e("myBury", "getMainBucketListFail : $it")
                 callback.fail()
             }
-
     }
-
-
-    @SuppressLint("CheckResult")
-    fun getMainBucketList2() {
-        if (accessToken == null || userId == null) {
-            _apiState.value = ApiState.Fail
-            return
-        }
-        _apiState.value = ApiState.Start
-        apiInterface.requestHomeBucketList(accessToken, userId, "all", "updatedDt")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ response ->
-                when (response.retcode) {
-                    "200" -> {
-                        Log.e("ayhan", "code")
-                        _apiState.value = ApiState.Success(response.bucketlists)
-                        _bucketList.value = response.bucketlists
-                    }
-                    "301" -> getRefreshToken(object : SimpleCallBack {
-                        override fun success() {
-                            _apiState.value = ApiState.Restart
-                        }
-
-                        override fun fail() {
-                            _apiState.value = ApiState.Fail
-                        }
-
-                    })
-                    else -> _apiState.value = ApiState.Fail
-                }
-            }) {
-                Log.e("myBury", "getMainBucketListFail : $it")
-                _apiState.value = ApiState.Fail
-            }
-
-    }
-
-
 }
