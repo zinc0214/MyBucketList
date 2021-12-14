d5160045d34a5a00da67ee2f4734929ab76fd827
hana <zinc0214@gmail.com>
2021-12-14 23:47:27 +0900

    [Home] 버킷리스트 전체정렬 > 하단에 여백을 위한 viewHolder 추가

--- app/src/main/java/womenproject/com/mybury/presentation/main/bucketlist/EditBucketListAdapter.kt
+++ app/src/main/java/womenproject/com/mybury/presentation/main/bucketlist/EditBucketListAdapter.kt
@@ -4,10 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import womenproject.com.mybury.data.BucketItem
+import womenproject.com.mybury.databinding.ItemBlankBinding
 import womenproject.com.mybury.databinding.ItemEditBucketBinding
 import womenproject.com.mybury.ui.ItemActionListener
 import womenproject.com.mybury.ui.ItemDragListener
 import womenproject.com.mybury.ui.ItemMovedListener
+import womenproject.com.mybury.util.Converter.Companion.dpToPx
 
 class EditBucketListAdapter(
     private val bucketList: List<BucketItem>,
@@ -15,35 +17,53 @@ class EditBucketListAdapter(
     private val itemMovedListener: ItemMovedListener
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>(), ItemActionListener {
 
-    private lateinit var editBucketListViewHolder: EditBucketListViewHolder
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
-        editBucketListViewHolder = EditBucketListViewHolder(
-            ItemEditBucketBinding.inflate(LayoutInflater.from(parent.context), parent, false),
-            dragListener
-        )
+        return if (viewType == 0) {
+            EditBucketListViewHolder(
+                ItemEditBucketBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+                dragListener
+            )
+        } else {
+            BlankViewHolder(
+                ItemBlankBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            )
+        }
+    }
 
-        return editBucketListViewHolder
+    override fun getItemViewType(position: Int): Int {
+        return if (position == bucketList.size) {
+            1
+        } else {
+            0
+        }
     }
 
     override fun getItemCount(): Int {
-        return bucketList.size
+        return bucketList.size + 1
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        editBucketListViewHolder.bind(bucketList[position])
+        when (holder) {
+            is EditBucketListViewHolder -> {
+                holder.bind(bucketList[position])
+            }
+            is BlankViewHolder -> {
+                holder.bind(dpToPx(60))
+            }
+        }
     }
 
     override fun onItemMoved(from: Int, to: Int) {
-        if (from == to) {
+        val toReal = if (to == 0) 0 else to - 1
+        if (from == toReal) {
             return
         }
 
         val bucketList = bucketList as MutableList<BucketItem>
         val fromItem = bucketList.removeAt(from)
-        bucketList.add(to, fromItem)
-        notifyItemMoved(from, to)
+        bucketList.add(toReal, fromItem)
+        notifyItemMoved(from, toReal)
 
         itemMovedListener.moved(bucketList)
     }
