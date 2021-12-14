394ddfe1d027af4fb504e05b238d8f52d241c3bc
hana <zinc0214@gmail.com>
2021-12-14 21:58:00 +0900

    [Home] > 검색 화면 > viewModel 개발

--- app/src/main/java/womenproject/com/mybury/presentation/main/search/SearchFragment.kt
+++ app/src/main/java/womenproject/com/mybury/presentation/main/search/SearchFragment.kt
@@ -1,37 +1,47 @@
 package womenproject.com.mybury.presentation.main.search
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import womenproject.com.mybury.R
+import womenproject.com.mybury.data.BucketItem
 import womenproject.com.mybury.data.SearchType
 import womenproject.com.mybury.databinding.FragmentSearchBinding
-import womenproject.com.mybury.presentation.base.BaseFragment
+import womenproject.com.mybury.presentation.main.bucketlist.MainBucketListAdapter
 import womenproject.com.mybury.presentation.viewmodels.SearchViewModel
 
-class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
+class SearchFragment : Fragment() {
 
-    override val layoutResourceId: Int
-        get() = R.layout.fragment_search
-
-    override val viewModel: SearchViewModel
-        get() = SearchViewModel()
-
+    private lateinit var viewModel: SearchViewModel
+    private lateinit var binding: FragmentSearchBinding
     private var selectedSearchType = SearchType.All
 
-    override fun initDataBinding() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+        viewModel = SearchViewModel()
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpViews()
+        setUpObserve()
     }
 
     private fun setUpViews() {
-        viewDataBinding.lifecycleOwner = this
-        viewDataBinding.apply {
+        binding.lifecycleOwner = this
+        binding.apply {
             searchType = selectedSearchType
 
             searchRadioGroup.setOnCheckedChangeListener { _, id ->
@@ -56,16 +66,26 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
                         "Type : ${selectedSearchType.getText()}",
                         Toast.LENGTH_SHORT
                     ).show()
+
+                    search("${textView.text}")
                 }
                 false
             }
 
-            closeButton.setOnClickListener { this@SearchFragment.onBackPressedFragment() }
+            closeButton.setOnClickListener { requireActivity().onBackPressed() }
         }
     }
 
-    fun onSelectedChipChanged(type: SearchType) {
-        viewDataBinding.searchType = type
-        viewDataBinding.notifyChange()
+    private fun search(word: String) {
+        viewModel.loadAllListSearch()
+    }
+
+    private fun setUpObserve() {
+        viewModel.allBucketSearchResult.observe(viewLifecycleOwner, allResultObserve)
+    }
+
+    private val allResultObserve = Observer<List<BucketItem>> {
+        binding.searchResultList.adapter =
+            MainBucketListAdapter(it, {})
     }
 }
--- app/src/main/java/womenproject/com/mybury/presentation/viewmodels/SearchViewModel.kt
+++ app/src/main/java/womenproject/com/mybury/presentation/viewmodels/SearchViewModel.kt
@@ -1,6 +1,68 @@
 package womenproject.com.mybury.presentation.viewmodels
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import womenproject.com.mybury.data.BucketItem
+import womenproject.com.mybury.data.Category
 import womenproject.com.mybury.presentation.base.BaseViewModel
 
 class SearchViewModel : BaseViewModel() {
+
+    private val _allBucketSearchResult = MutableLiveData<List<BucketItem>>()
+    val allBucketSearchResult: LiveData<List<BucketItem>> = _allBucketSearchResult
+
+    fun loadAllListSearch() {
+        if (accessToken == null || userId == null) {
+            return
+        }
+
+        viewModelScope.launch(Dispatchers.IO) {
+            withContext(Dispatchers.Main) {
+                try {
+                    _allBucketSearchResult.value = items()
+                    Log.e("ayhan", "here : ${_allBucketSearchResult.value}")
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                    withContext(Dispatchers.Main) {
+                        // do nothing
+                    }
+                }
+            }
+        }
+    }
+
+    private fun items(): List<BucketItem> {
+        return listOf(
+            BucketItem(
+                id = "1",
+                category = Category(
+                    name = "H",
+                    id = "",
+                    priority = 0
+                ),
+                memo = "",
+                dDay = 10,
+                title = "12346"
+            ),
+            BucketItem(
+                id = "1",
+                category = Category(
+                    name = "H",
+                    id = "",
+                    priority = 0
+                ),
+                userCount = 10,
+                goalCount = 20,
+                memo = "",
+                dDay = 10,
+                title = "12346"
+            )
+        )
+    }
+
 }
