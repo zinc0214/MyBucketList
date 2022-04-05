3e8858e9bc45c91e29da179171442a6e26e0df1b
hana <zinc0214@gmail.com>
2022-04-05 21:41:08 +0900

    [Release] 1.5.0 (build 20)

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -6,8 +6,8 @@ object Versions {
 
     const val minSdk = 23
     const val targetSdk = 31
-    const val versionCode = 19
-    const val versionName = "1.4.6"
+    const val versionCode = 20
+    const val versionName = "1.5.0"
 }
 
 object Dep {
