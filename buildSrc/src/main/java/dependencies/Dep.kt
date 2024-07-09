2b84d2bf353055b8ec1eb7ed6719b42614268a97
zinc0214 <zinc0214@gmail.com>
2024-07-09 22:54:01 +0900

    제거되지 않은 Dep 코드 제거

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -2,9 +2,9 @@ package dependencies
 
 object Versions {
     const val compileSdk = 34
-
     const val minSdk = 23
     const val targetSdk = 33
     const val versionCode = 32
     const val versionName = "1.7.1"
 }
+
