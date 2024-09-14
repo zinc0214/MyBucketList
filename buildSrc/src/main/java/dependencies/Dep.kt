1a65ea1bbd0cd9a83eaf3256e4436189f4e656b1
zinc0214 <zinc0214@gmail.com>
2024-09-14 23:52:35 +0900

    target sdk 34

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -3,7 +3,7 @@ package dependencies
 object Versions {
     const val compileSdk = 34
     const val minSdk = 23
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val versionCode = 33
     const val versionName = "1.8.1"
 }
