9699a0bd39489cec1c57642c27b573bfeb1b20e3
zinc0214 <zinc0214@gmail.com>
2024-09-14 23:49:42 +0900

    [TestRelease] 1.8.1 (33)

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -4,6 +4,6 @@ object Versions {
     const val compileSdk = 34
     const val minSdk = 23
     const val targetSdk = 33
-    const val versionCode = 32
-    const val versionName = "1.7.1"
+    const val versionCode = 33
+    const val versionName = "1.8.1"
 }
