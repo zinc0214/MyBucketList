b4eabb2d02400528f502564181722b22adc98dca
zinc0214 <zinc0214@gmail.com>
2026-08-04 22:17:12 +0900

    release 1.8.10(42)

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -4,6 +4,6 @@ object Versions {
     const val compileSdk = 37
     const val minSdk = 23
     const val targetSdk = 37
-    const val versionCode = 41
-    const val versionName = "1.8.9"
+    const val versionCode = 42
+    const val versionName = "1.8.10"
 }
